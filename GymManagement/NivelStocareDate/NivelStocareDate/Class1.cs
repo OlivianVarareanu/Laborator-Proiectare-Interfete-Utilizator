6f2ea00cee9b27da1396ca9846f36139b9dd7a45
OlivianVarareanu <106438300+OlivianVarareanu@users.noreply.github.com>
2023-03-28 21:43:07 +0300

    Add files via upload
    -------FIX-------
    -refacerea temei pentru laboratorul 3

---
+++ GymManagement/NivelStocareDate/NivelStocareDate/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using LibrarieModele;
+
+namespace NivelStocareData
+{
+    public static class DataManager
+    {
+        public static void SaveAllData(Gym gym)
+        {
+            string membersFilePath = @"members.txt";
+            string trainersFilePath = @"trainers.txt";
+            string equipmentFilePath = @"equipment.txt";
+
+            // Save members to file
+            using (StreamWriter writer = new StreamWriter(membersFilePath))
+            {
+                foreach (Member member in gym.Members)
+                {
+                    writer.WriteLine($"{member.Name},{member.Age},{member.Email}");
+                }
+            }
+
+            // Save trainers to file
+            using (StreamWriter writer = new StreamWriter(trainersFilePath))
+            {
+                foreach (Trainer trainer in gym.Trainers)
+                {
+                    writer.WriteLine($"{trainer.Name},{trainer.Specialization}");
+                }
+            }
+
+            // Save equipment to file
+            using (StreamWriter writer = new StreamWriter(equipmentFilePath))
+            {
+                foreach (Equipment equipment in gym.Equipment)
+                {
+                    writer.WriteLine($"{equipment.Name},{equipment.Type},{equipment.Quantity}");
+                }
+            }
+        }
+
+    }
+}

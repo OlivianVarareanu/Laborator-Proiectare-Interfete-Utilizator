0addabce5185673a0cc6a87c467c78b6b5b343b3
OlivianVarareanu <106438300+OlivianVarareanu@users.noreply.github.com>
2023-03-28 22:10:53 +0300

    Add files via upload
    TEMA LAB 4

---
+++ Tema/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[][] words = new string[26][];
+        for (int i = 0; i < 26; i++)
+        {
+            words[i] = new string[0];
+        }
+
+        foreach (string arg in args)
+        {
+            char firstLetter = arg.ToLower()[0];
+            int index = firstLetter - 'a';
+            Array.Resize(ref words[index], words[index].Length + 1);
+            words[index][words[index].Length - 1] = arg;
+        }
+
+        for (int i = 0; i < 26; i++)
+        {
+            char letter = (char)('a' + i);
+            Console.WriteLine("Words starting with {0}:", letter);
+            Console.WriteLine(string.Join(", ", words[i]));
+        }
+    }
+}
